f17145d4af0b692352a41969c60ee84445f0da5f
a-reznikov <a.reznnikov@gmail.com>
2025-04-06 17:53:12 +0300

    fix: setup security groups

--- cart-service/lib/cart-service-stack.ts
+++ cart-service/lib/cart-service-stack.ts
@@ -3,6 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Code, Runtime, Function } from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as path from "path";
 
 dotenv.config();
@@ -16,6 +17,8 @@ export class AReznikovCartServiceStack extends cdk.Stack {
     const postgresUsername = process.env.POSTGRES_USERNAME;
     const postgresPassword = process.env.POSTGRES_PASSWORD;
     const postgresDatabase = process.env.POSTGRES_DATABASE;
+    const vpcId = process.env.VPC_ID;
+    const rdsSecurityGroupId = process.env.RDS_SECURITY_GROUP_ID;
 
     if (
       !(
@@ -23,7 +26,9 @@ export class AReznikovCartServiceStack extends cdk.Stack {
         postgresPort &&
         postgresUsername &&
         postgresPassword &&
-        postgresDatabase
+        postgresDatabase &&
+        vpcId &&
+        rdsSecurityGroupId
       )
     ) {
       throw Error(
@@ -31,10 +36,42 @@ export class AReznikovCartServiceStack extends cdk.Stack {
       );
     }
 
+    const vpc = ec2.Vpc.fromLookup(this, "AReznikovVPC", {
+      vpcId: vpcId,
+    });
+
+    const rdsSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+      this,
+      "AReznikovRDSSecurityGroup",
+      rdsSecurityGroupId
+    );
+
+    const lambdaSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "AReznikovLambdaSecurityGroup",
+      {
+        vpc,
+        description: "Security group for Lambda to access RDS",
+        allowAllOutbound: true,
+      }
+    );
+
+    rdsSecurityGroup.addIngressRule(
+      lambdaSecurityGroup,
+      ec2.Port.tcp(5432),
+      "Allow Lambda to connect to RDS"
+    );
+
     const cartLambda = new Function(this, "AReznikovCartLambda", {
       runtime: Runtime.NODEJS_20_X,
       handler: "lambda.handler",
       code: Code.fromAsset(path.join(__dirname, "../../nest/dist")),
+      vpc,
+      securityGroups: [lambdaSecurityGroup],
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      allowPublicSubnet: true,
       environment: {
         POSTGRES_HOST: postgresHost,
         POSTGRES_PORT: postgresPort,
@@ -42,7 +79,8 @@ export class AReznikovCartServiceStack extends cdk.Stack {
         POSTGRES_PASSWORD: postgresPassword,
         POSTGRES_DATABASE: postgresDatabase,
       },
-      timeout: cdk.Duration.seconds(15),
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 256,
     });
 
     new apigateway.LambdaRestApi(this, "AReznikovCartApiGateway", {
